b385f0ff848badf3a3c52f4eb94803767f20787e
nadiasal2 <heyyosaaa@gmail.com>
2024-05-07 08:11:48 +0700

    add login_page

--- src/views/login_page/Login.jsx
+++ src/views/login_page/Login.jsx
@@ -1,9 +1,36 @@
+import React from "react";
 
+// const Navbar = () => {
+//   return (
+//     <div className="Navbar">
+//       <img src="" alt="" className="logo"></img>
+//       <ul>
+//           <li>Login</li>
+//       </ul>
+//     </div>
+//   );
+// };
 
 const Login = () => {
   return (
-      <h1>Login to your account</h1>
-  )
-}
+      <div>
+        <Navbar/> {}
+        <div className="login-form bg-yellow-600">
+          <h1 className="text-4x1 text-blackfont-bold text-center mb-6">Login</h1>
+          <form action="">
+          <div className="relative my-4">
+            <input type="email"/>
+            <label htmlFor="">Your Email</label>
+          </div>
+          <div relative my-4>
+            <input type="password" className="block w-27 py-2.3 px-0 text-sm text-black"/>
+            <label htmlFor="">Your Password</label>
+          </div>
+          <button type="submit">Login</button>
+          </form>
+        </div>
+      </div>
+  );
+};
 
 export default Login
---
+++ src/views/login_page/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import '../../assets/profile1.png';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+      <div className="Navbar">
+        <img src="" alt="" className="logo"></img>
+        <ul>
+            <li>Login</li>
+        </ul>
+
+        <div className="login">
+            <img src="../../profile1.png" alt="" ></img>
+            
+        </div>
+      </div>
+    );
+  };
