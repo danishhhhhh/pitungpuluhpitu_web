62cb560c2483cd56e0067cca75082e1a72ca7046
Strenousrascal02903 <dahlah0108@gmail.com>
2024-05-08 07:49:14 +0700

    feat: [NA] add responsive

--- src/component/Navbar/navbar.jsx
+++ src/component/Navbar/navbar.jsx
@@ -3,12 +3,12 @@ import Clock from "../../component/Navbar/clock"
 
 const Navbar = () => {
   return (
-    <div className="bg-lightyellow w-full h-20 fixed top-0 left-80 flex justify-start items-center">
+    <div className="bg-lightyellow w-full h-20 fixed top-0 2xl:left-80 xl:left-60 flex justify-start items-center">
       <div className="mx-12 my-6">
         <h1 className="font-poppins text-left font-semibold text-xl">Data Stock</h1>
       </div>
-      <div className="w-80 ml-80"></div>
-      <div className=" ml-32"></div>
+      <div className="w-80 2xl:w-80 2xl:ml-80 ml-80 lg:w-10"></div>
+      <div className=" ml-32 2xl:ml-32  xl:ml-6"></div>
       <div className="flex justify-end ml-80 ">
         <Clock/>
       </div>
