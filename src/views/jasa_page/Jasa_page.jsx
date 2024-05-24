a9a7a63d6fcb99c62291a94536d66939c0a241f9
nadiasal2 <heyyosaaa@gmail.com>
2024-05-24 22:18:19 +0700

    feat: [NS] add jasa dashboard

--- src/views/jasa_page/Jasa_page.jsx
+++ src/views/jasa_page/Jasa_page.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
-import Sidebar from "../../component/Sidebar/sidebar";
-import Navbar from "../../component/Navbar/navbar";
-const JasaPage = () => {
-  return (
-    <div className="flex ">
-    <Sidebar />
-    <Navbar data="Data Jasa"/>
+import React, {useState} from 'react'
+import Sidebar from "../../component/Sidebar/sidebar.jsx";
+import Navbar from "../../component/Navbar/navbar.jsx";
+import DefaultSecondaryTable from "../../component/Table/DefaultSecondaryTable.jsx";
+import DetailMainTable from "../../component/Table/DetailMainTable.jsx";
+import DefaultMainTable from '../../component/Table/DefaultMainTable.jsx';
 
-    </div>
-  )
+const JasaPage = () => {
+    const [jasa, setJasa] = useState([
+        { nama: "Jasa 1" },
+        { nama: "Jasa 2" },
+        { nama: "Jasa 3" },
+    ]);
+
+    return (
+        <div className='min-h-screen flex flex-row'>
+            <Sidebar/>
+            <div className="flex-grow">
+                <Navbar data="Detail Tim 1" showBackButton={true}/>
+                <div className="p-12 flex flex-row justify-center">
+                    <div className="w-3/5">
+                    <DefaultMainTable name={"Jasa"} data={jasa} setData={setJasa}/>
+                    </div>
+                    <div className="w-8"/>
+                  
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default JasaPage
+export default JasaPage;
