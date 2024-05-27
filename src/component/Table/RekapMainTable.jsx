6c19b0beed5c866910a17db8b666aa8da7f1aeba
Strenousrascal02903 <dahlah0108@gmail.com>
2024-05-27 18:53:01 +0700

    feat: [NA] fetch rekap api

--- src/component/Table/RekapMainTable.jsx
+++ src/component/Table/RekapMainTable.jsx
@@ -3,153 +3,98 @@ import "../../App.css";
 import Pagination from "./Pagination.jsx";
 import { FaSearch } from "react-icons/fa";
 
-const RekapMainTable = () => {
-    const spareparts = [
-        {
-            NamaCabang: "Kalisat (Jember)",
-            namaMekanik: "Moh. Dani",
-            platNomor: "K 9998 HB",
-            tipeMotor: "Beat",
-            tahunMotor: 2021,
-            kmMotor: 99999,
-            Pekerjaan: "Servis Rutin",
-            jasa: "Setting Throttle Position (TP), Stel Velg Belakang",
-            sparepart:
-                "Ban Tubeless Belakang Matic, Ban Tubeless Belakang Verza, Kanvas Rem Belakang Matic, Ban Tubeless Belakang Matic, Ban Tubeless Belakang Verza ",
-            tanggal: "18-12-2024",
-        },
-        {
-            NamaCabang: "Kalisat (Jember)",
-            namaMekanik: "Moh. Dani",
-            platNomor: "K 9998 HB",
-            tipeMotor: "Beat",
-            tahunMotor: 2021,
-            kmMotor: 99999,
-            Pekerjaan: "Servis Rutin",
-            jasa: "Setting Throttle Position (TP), Stel Velg Belakang",
-            sparepart:
-                "Ban Tubeless Belakang Matic, Ban Tubeless Belakang Verza, Kanvas Rem Belakang Matic, Ban Tubeless Belakang Matic, Ban Tubeless Belakang Verza ",
-            tanggal: "18-12-2024",
-        },
-        {
-            NamaCabang: "Kalisat (Jember)",
-            namaMekanik: "Moh. Dani",
-            platNomor: "K 9998 HB",
-            tipeMotor: "Beat",
-            tahunMotor: 2021,
-            kmMotor: 99999,
-            Pekerjaan: "Servis Rutin",
-            jasa: "Setting Throttle Position (TP), Stel Velg Belakang",
-            sparepart:
-                "Ban Tubeless Belakang Matic, Ban Tubeless Belakang Verza, Kanvas Rem Belakang Matic, Ban Tubeless Belakang Matic, Ban Tubeless Belakang Verza ",
-            tanggal: "18-12-2024",
-        },
-        {
-            NamaCabang: "Kalisat (Jember)",
-            namaMekanik: "Moh. Dani",
-            platNomor: "K 9998 HB",
-            tipeMotor: "Beat",
-            tahunMotor: 2021,
-            kmMotor: 99999,
-            Pekerjaan: "Servis Rutin",
-            jasa: "Setting Throttle Position (TP), Stel Velg Belakang",
-            sparepart:
-                "Ban Tubeless Belakang Matic, Ban Tubeless Belakang Verza, Kanvas Rem Belakang Matic, Ban Tubeless Belakang Matic, Ban Tubeless Belakang Verza ",
-            tanggal: "18-12-2024",
-        },
-    ];
-
-    return (
-        <div className="grid grid-rows-1 ">
-            <div className="flex justify-between items-center">
-                <div className="flex flex-row justify-between bg-lightgrey rounded-lg w-1/4">
-                    <input
-                        type="text"
-                        placeholder="Cari sparepart ..."
-                        className="w-64 py-2 px-4 bg-lightgrey rounded-lg text-darkgrey focus:outline-none font-poppins text-sm"
-                    />
-                    <FaSearch className="my-auto mx-4  text-darkgrey" />
-                </div>
-            </div>
-            <div className="h-4" />
-            <div className="flex flex-col">
-                <table className="border-collapse border-gray-300 rounded-lg">
-                    <thead>
-                    <tr className="text-black">
-                        <th className="py-3 font-poppins border border-grey text-darkgrey font-medium">
-                            Nama Cabang
-                        </th>
-                        <th className="px-3 py-3 font-poppins border border-grey text-darkgrey font-medium">
-                            Nama Mekanik
-                        </th>
-                        <th className="px-3 py-3 font-poppins border border-grey text-darkgrey font-medium">
-                            Plat Nomor
-                        </th>
-                        <th className="px-3 py-3 font-poppins border border-grey text-darkgrey font-medium">
-                            Tipe Motor
-                        </th>
-                        <th className="px-3 py-3 font-poppins border border-grey text-darkgrey font-medium">
-                            Tahun Motor
-                        </th>
-                        <th className="px-3 py-3 font-poppins border border-grey text-darkgrey font-medium">
-                            Km Motor
-                        </th>
-                        <th className="px-3 py-3 font-poppins border border-grey text-darkgrey font-medium">
-                            Pekerjaan
-                        </th>
-                        <th className="px-3 py-3 font-poppins border border-grey text-darkgrey font-medium">
-                            Jasa
-                        </th>
-                        <th className="px-3 py-3 font-poppins border border-grey text-darkgrey font-medium">
-                            Sparepart
-                        </th>
-                        <th className="px-3 py-3 font-poppins border border-grey text-darkgrey font-medium">
-                            Tanggal
-                        </th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {spareparts.map((sparepart, index) => (
-                        <tr key={index}>
-                            <td className="px-3 py-2 font-poppins border border-grey">
-                                {sparepart.NamaCabang}
-                            </td>
-                            <td className="px-3 py-2 font-poppins border border-grey">
-                                {sparepart.namaMekanik}
-                            </td>
-                            <td className="px-3 py-2 font-poppins border border-grey">
-                                {sparepart.platNomor}
-                            </td>
-                            <td className="px-3 py-2 font-poppins border border-grey">
-                                {sparepart.tipeMotor}
-                            </td>
-                            <td className="px-3 py-2 font-poppins border border-grey">
-                                {sparepart.tahunMotor}
-                            </td>
-                            <td className="px-3 py-2 font-poppins border border-grey">
-                                {sparepart.kmMotor}
-                            </td>
-                            <td className="px-3 py-2 font-poppins border border-grey">
-                                {sparepart.Pekerjaan}
-                            </td>
-                            <td className="px-3 py-2 font-poppins border border-grey">
-                                {sparepart.jasa}
-                            </td>
-                            <td className="px-3 py-2 font-poppins border border-grey">
-                                {sparepart.sparepart}
-                            </td>
-                            <td className="px-3 py-2 font-poppins border border-grey">
-                                {sparepart.tanggal}
-                            </td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-            </div>
-            <div className="h-4" />
-            <Pagination />
+const RekapMainTable = ({ name, data }) => {
+  return (
+    <div className="grid grid-rows-1 ">
+      <div className="flex justify-between items-center">
+        <div className="flex flex-row justify-between bg-lightgrey rounded-lg w-1/3">
+          <input
+            type="text"
+            placeholder={`Cari ${name.toLowerCase()} ...`}
+            className="w-64 py-2 px-4 bg-lightgrey rounded-lg text-darkgrey focus:outline-none font-poppins text-sm"
+          />
+          <FaSearch className="my-auto mx-4 text-darkgrey" />
         </div>
-    );
+      </div>
+      <div className="h-6" />
+      <div className="flex flex-col">
+        <table className="border-collapse border-gray-300 rounded-lg">
+          <thead>
+            <tr className="text-black">
+              <th className="py-3 font-poppins border border-grey text-darkgrey font-medium">
+                Nama Cabang
+              </th>
+              <th className="px-3 py-3 font-poppins border border-grey text-darkgrey font-medium">
+                Nama Mekanik
+              </th>
+              <th className="px-3 py-3 font-poppins border border-grey text-darkgrey font-medium">
+                Plat Nomor
+              </th>
+              <th className="px-3 py-3 font-poppins border border-grey text-darkgrey font-medium">
+                Tipe Motor
+              </th>
+              <th className="px-3 py-3 font-poppins border border-grey text-darkgrey font-medium">
+                Tahun Motor
+              </th>
+              <th className="px-3 py-3 font-poppins border border-grey text-darkgrey font-medium">
+                Km Motor
+              </th>
+              <th className="px-3 py-3 font-poppins border border-grey text-darkgrey font-medium">
+                Pekerjaan
+              </th>
+              <th className="px-3 py-3 font-poppins border border-grey text-darkgrey font-medium">
+                Jasa
+              </th>
+              <th className="px-3 py-3 font-poppins border border-grey text-darkgrey font-medium">
+                Sparepart
+              </th>
+              <th className="px-3 py-3 font-poppins border border-grey text-darkgrey font-medium">
+                Tanggal
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((sparepart, index) => (
+              <tr key={index}>
+                <td className="px-3 py-2 font-poppins border border-grey">
+                  {sparepart.cabang}
+                </td>
+                <td className="px-3 py-2 font-poppins border border-grey">
+                  {sparepart.mekanik}
+                </td>
+                <td className="px-3 py-2 font-poppins border border-grey">
+                  {sparepart.plat_motor}
+                </td>
+                <td className="px-3 py-2 font-poppins border border-grey">
+                  {sparepart.motor}
+                </td>
+                <td className="px-3 py-2 font-poppins border border-grey">
+                  {sparepart.tahun}
+                </td>
+                <td className="px-3 py-2 font-poppins border border-grey">
+                  {sparepart.km_motor}
+                </td>
+                <td className="px-3 py-2 font-poppins border border-grey">
+                  {sparepart.pekerjaan}
+                </td>
+                <td className="px-3 py-2 font-poppins border border-grey">
+                  {sparepart.jasa}
+                </td>
+                <td className="px-3 py-2 font-poppins border border-grey">
+                  {sparepart.sparepart}
+                </td>
+                <td className="px-3 py-2 font-poppins border border-grey">
+                  {sparepart.tanggal}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="h-6" />
+      <Pagination />
+    </div>
+  );
 };
 
-export default RekapMainTable
+export default RekapMainTable;
--- src/views/rekap_page/Rekap_page.jsx
+++ src/views/rekap_page/Rekap_page.jsx
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../../component/Navbar/navbar";
 
 import RekapMainTable from "../../component/Table/RekapMainTable.jsx";
+import { getRekapRequest } from "../../features/Rekap.jsx";
+
 
 const RekapPage = () => {
+
+
+  const [rekap, setRekap] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const responseRekap = await getRekapRequest();
+
+        setRekap(responseRekap.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <div className="min-h-screen flex flex-row">
       <div className="flex-grow">
         <Navbar data="Rekap Pengerjaan Tim 1" showBackButton={true} />
         <div className="p-12 flex flex-row justify-start">
           <div className="w-full">
-            <RekapMainTable />
+            <RekapMainTable  name={"Rekap"}
+              data={rekap}
+              setData={setRekap} />
           </div>
           <div className="w-8" />
         </div>
