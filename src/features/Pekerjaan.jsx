756ab66f44a06e2e9d4ef5efd102be1fb4e963c3
Strenousrascal02903 <dahlah0108@gmail.com>
2024-05-28 15:53:58 +0700

    feat: [NA] fetch post ,edit, delete pekerjaan api

--- src/features/Pekerjaan.jsx
+++ src/features/Pekerjaan.jsx
@@ -22,3 +22,51 @@ export const getPekerjaanRequest = async (page = 1) => {
 
   return response;
 };
+
+export const postAddPekerjaanRequest = async (name, timId) => {
+  const token = localStorage.getItem("token");
+  console.log(token);
+  const additionalHeaders = {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`,
+  };
+
+  console.log(additionalHeaders);
+  const response = await apiRequest("POST", `/pekerjaan`, { name }, additionalHeaders);
+
+  console.log(response.data);
+
+  return response;
+};
+
+export const postEditPekerjaanRequest = async (name,id) => {
+  const token = localStorage.getItem("token");
+  console.log(token);
+  const additionalHeaders = {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`,
+  };
+
+  console.log(additionalHeaders);
+  const response = await apiRequest("POST", `/pekerjaan/${id}`, { name }, additionalHeaders);
+
+  console.log(response.data);
+
+  return response;
+};
+
+export const deletePekerjaanRequest = async (id) => {
+  const token = localStorage.getItem("token");
+  console.log(token);
+  const additionalHeaders = {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`,
+  };
+
+  console.log(additionalHeaders);
+  const response = await apiRequest("DELETE", `/pekerjaan/${id}`, null, additionalHeaders);
+
+  console.log(response.data);
+
+  return response;
+};
--- src/views/pekerjaan_page/Pekerjaan_page.jsx
+++ src/views/pekerjaan_page/Pekerjaan_page.jsx
@@ -4,14 +4,20 @@ import Navbar from "../../component/Navbar/navbar.jsx";
 import DefaultSecondaryTable from "../../component/Table/DefaultSecondaryTable.jsx";
 import DetailMainTable from "../../component/Table/DetailMainTable.jsx";
 import DefaultMainTable from "../../component/Table/DefaultMainTable.jsx";
-import { getPekerjaanRequest } from "../../features/Pekerjaan.jsx";
-
+import {
+  getPekerjaanRequest,
+  postAddPekerjaanRequest,
+  postEditPekerjaanRequest,
+  deletePekerjaanRequest,
+} from "../../features/Pekerjaan.jsx";
 
 const PekerjaanPage = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [totalData, setTotalData] = useState();
   const [totalPage, setTotalPage] = useState();
   const [pekerjaan, setPekerjaan] = useState([]);
+  const [pekerjaanValue, setPekerjaanValue] = useState();
+
 
   const fetchData = async (page = 1) => {
     try {
@@ -22,8 +28,32 @@ const PekerjaanPage = () => {
       setTotalPage(responsePekerjaan.total_page);
 
       setPekerjaan(responsePekerjaan.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
+  const postAddPekerjaan = async () => {
+    try {
+      await postAddPekerjaanRequest(pekerjaanValue);
+      console.log("sadasdsadasdasdsa");
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
+  const postEditPekerjaan = async (id) => {
+    try {
+      await postEditPekerjaanRequest(pekerjaanValue, id);
+      console.log("sadasdsadasdasdsa");
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  const deletePekerjaan = async (id) => {
+    try {
+      await deletePekerjaanRequest(id);
+      console.log("sadasdsadasdasdsa");
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -47,6 +77,11 @@ const PekerjaanPage = () => {
               totalData={totalData}
               totalPage={totalPage}
               setCurrentPage={setCurrentPage}
+              value={pekerjaanValue}
+              setValue={setPekerjaanValue}
+              handleSubmitPost={postAddPekerjaan}
+              handleEditPost={postEditPekerjaan}
+              handleDeletePost={deletePekerjaan}
             />
           </div>
           <div className="w-8" />
